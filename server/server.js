b1ad8a29cd69c4465d9450dc4c822387f9ccad4c
Anirudha Patil <95077620+Aniruddha1701@users.noreply.github.com>
2023-11-19 09:31:09 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,6 +11,6 @@ app.use("/api/rooms", roomsRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/bookings", bookingRoute);
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Node app listening on ${port} port!`));
